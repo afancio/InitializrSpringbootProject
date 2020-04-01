0cb3441109a9ffef8578777f28882271643b28c6
andre <andre@ufop.edu.br>
2020-04-01 11:52:43 -0300

    Tesatando a primeira classe de domínio

--- src/main/java/com/example/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/example/cursomc/resources/CategoriaResource.java
@@ -5,6 +5,9 @@
  */
 package com.example.cursomc.resources;
 
+import com.example.domain.Categoria;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
  * @author nti
  */
 @RestController //vai ser um controlador Rest
-@RequestMapping(value="/categorias") //e vai responder por esse end point aqui
+@RequestMapping(value="/categorias") //e vai responder por esse end point aqui ... digitar depois do localHost:8081
 public class CategoriaResource {
  
     @RequestMapping(method=RequestMethod.GET)
-    public String listar(){
-        return "REST está funcionando";
+    public List<Categoria> listar(){
+        
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+        
+        //list é uma interface e não pode ser instanciado...
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+        
+        return lista;
     }
 }
---
+++ src/main/java/com/example/domain/Categoria.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author nti
+ */
+//Objetos possam ser convertidos em sequencia de bite.. para permitir traáfego na web
+public class Categoria implements Serializable{
+    private static final long SerialVersionUID = 1L;
+    
+    private Integer id;
+    private String nome;
+
+    public Categoria() {
+    }
+
+    public Categoria(Integer id, String nome) {
+        super();
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    //hashCode - para que dois objetos sejam comparados pelo seu conteúdo 
+    //e não pelo seu conteúdo do ponteiro
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 17 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Categoria other = (Categoria) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+    
+}
